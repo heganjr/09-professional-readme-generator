f1ea00f00c1d30d6391f63155e054d0c5c997666
heganjr <jacobhegan@hotmail.com>
2021-05-08 01:05:49 +0800

    added starter questions added licence badges to readme generation added licence text to readme generation

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,49 @@
 // TODO: Include packages needed for this application
+const generateMarkdown = require(`./utils/generateMarkdown`);
+// importing the file to index.js
+
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+// allows to to manipulate the readMe generator (ie. create it)
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is the name of your project?",
+    default: "Insert project name here",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "What licence would you like to use?",
+    choices: ["MIT", "GNU", "Apache", "BSD-3", "None"],
+  },
+
+  {
+      type: "input",
+      name: "fullName",
+      message: "What is your name?",
+      default: "Insert your full name here",
+  },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) =>
+    err ? console.error(err) : console.log("Success!")
+  );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions).then((data) => {
+      writeToFile("readme.md", generateMarkdown(data) )
+  });
+}
 
+// write file - call it readme.md and generate based on generateMarkdown - data
 // Function call to initialize app
 init();
